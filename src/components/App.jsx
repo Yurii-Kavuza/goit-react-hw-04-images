9f4f2b461166e7613b64677175924e2c5c2b33ea
Yurii-Kavuza <kavuzaiurii@gmail.com>
2022-11-04 22:18:38 +0200

    intermediate stage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import { nanoid } from 'nanoid';
 import Section from './Section';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
+import SearchBar from './SearchBar';
+import { Container } from './App.styled';
+
+const API_KEY = '29343329-eb32098cf47fcc64118c9b881';
+const OPTIONS = 'image_type=photo&orientation=horizontal&per_page=12';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 class App extends React.Component {
   static propTypes = {
@@ -19,71 +27,29 @@ class App extends React.Component {
   };
 
   state = {
-    contacts: [],
-    filter: '',
+    images: [],
+    search: '',
   };
 
-  deleteContact = dataId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== dataId),
-    }));
-  };
-
-  formSubmitHandler = data => {
-    data.id = nanoid();
-    const contacts = this.state.contacts;
-    const containsNamee = contacts.find(contact => contact.name === data.name);
-    if (containsNamee) {
-      return alert(`${data.name} is already in contacts.`);
-    }
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, data],
-    }));
-  };
-
-  filteredContacts = () => {
-    const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
+  async componentDidMount() {
+    const response = await axios.get(
+      `?q=${this.state.search}&page=1&key=${API_KEY}&${OPTIONS}`
     );
-  };
-
-  onFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
+    this.setState({ images: response.data });
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
+  componentDidUpdate(prevProps, prevState) {}
+
+  formSubmitHandler(searchQuery) {
+    console.log(searchQuery);
   }
 
   render() {
     const { filter } = this.state;
     return (
-      <div>
-        <Section title={'Phonebook'}>
-          <ContactForm onSubmit={this.formSubmitHandler} />
-        </Section>
-        <Section title={'Contacts'}>
-          <Filter onFilter={this.onFilter} value={filter} />
-          <ContactList
-            contacts={this.filteredContacts()}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
-      </div>
+      <Container>
+        <SearchBar onSubmit={this.formSubmitHandler} />
+      </Container>
     );
   }
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,53 @@
 import { Component } from 'react';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import {
+  Button,
+  ButtonLabel,
+  FormError,
+  FormBox,
+  Header,
+  Input,
+} from './SearchBar.styled';
+import { AiOutlineSearch } from 'react-icons/ai';
 
-class SearchBar extends Component{
+class SearchBar extends Component {
+  schema = yup.object().shape({
+    search: yup.string().min(2).required(),
+  });
 
+  initialValues = { search: '' };
+
+  handleSubmit = (values, { resetForm }) => {
+    this.props.onSubmit(values);
+    resetForm();
+  };
+
+  render() {
+    return (
+      <Header>
+        <Formik
+          initialValues={this.initialValues}
+          onSubmit={this.handleSubmit}
+          validationSchema={this.schema}
+        >
+          <FormBox autoComplete="off">
+            <Button type="submit">
+              <AiOutlineSearch />
+            </Button>
+
+            <Input
+              name="search"
+              type="text"
+              autoFocus
+              placeholder="Search images and photos"
+            />
+            <FormError name="search" component="div" />
+          </FormBox>
+        </Formik>
+      </Header>
+    );
+  }
 }
 
 export default SearchBar;
