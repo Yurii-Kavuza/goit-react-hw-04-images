ef1493c38f6a8177f2c55ef3e2cbee20311b8952
Yurii-Kavuza <kavuzaiurii@gmail.com>
2022-12-02 20:59:57 +0200

    rework App.jsx with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import * as API from 'services/api';
 import { Container } from './App.styled';
@@ -9,151 +9,126 @@ import Button from './Button';
 import Modal from './Modal';
 import Loader from './Loader';
 
-class App extends React.Component {
-  static propTypes = {
-    images: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        largeImageURL: PropTypes.string,
-        previewURL: PropTypes.string,
-        tags: PropTypes.string.isRequired,
-      })
-    ),
-    totalImages: PropTypes.number,
-    search: PropTypes.string,
-    isLoading: PropTypes.bool,
-    error: PropTypes.string,
-    isButtonShown: PropTypes.bool,
-    page: PropTypes.number,
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [totalImages, setTotalImages] = useState(0);
+  const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isButtonShown, setIsButtonShown] = useState(false);
+  const [page, setPage] = useState(1);
+  const [isModalShown, setIsModalShown] = useState(false);
+  const [largeImage, setLargeImage] = useState(null);
+  const [desc, setDesc] = useState('');
+
+  const setDefaultData = () => {
+    setImages([]);
+    setTotalImages(0);
+    setSearch('');
+    setIsLoading(false);
+    setError(null);
+    setIsButtonShown(false);
+    setPage(1);
+    setIsModalShown(false);
+    setLargeImage(null);
+    setDesc('');
   };
 
-  state = {
-    images: [],
-    totalImages: 0,
-    search: '',
-    isLoading: false,
-    error: null,
-    isButtonShown: false,
-    page: 1,
-    isModalShown: false,
-    largeImage: null,
-    desc: '',
+  const formSubmitHandler = async searchQuery => {
+    setDefaultData();
+    setSearch(searchQuery.search);
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.page !== this.state.page ||
-      prevState.search !== this.state.search
-    ) {
-      this.loadPictures();
-    }
-    if (prevState.images !== this.state.images) {
-      this.setState({
-        isButtonShown: this.checkButtonShow(),
-      });
-    }
-  }
-
-  setDefaultData = () => {
-    this.setState({
-      images: [],
-      totalImages: 0,
-      search: '',
-      isLoading: false,
-      error: null,
-      isButtonShown: false,
-      page: 1,
-      isModalShown: false,
-      largeImage: null,
-      desc: '',
-    });
+  const setModalImageData = (largeImage, desc) => {
+    setLargeImage(largeImage);
+    setDesc(desc);
+    setIsModalShown(true);
   };
 
-  formSubmitHandler = async searchQuery => {
-    this.setDefaultData();
-    this.setState({ search: searchQuery.search });
+  const clearModalImageData = () => {
+    setLargeImage(null);
+    setDesc('');
   };
 
-  setModalImageData = (largeImage, desc) => {
-    this.setState({ largeImage, desc, isModalShown: true });
-  };
-
-  clearModalImageData = () => {
-    this.setState({ largeImage: null, desc: '' });
-  };
-
-  loadPictures = async () => {
-    const { search, page } = this.state;
-    try {
-      this.setState({ isLoading: true });
-      const { hits, totalHits } = await API.fetchImages(search, page);
-      this.setState(state => ({
-        images: [...state.images, ...hits],
-        totalImages: totalHits,
-      }));
-      this.setState({ isButtonShown: this.checkButtonShow() });
-    } catch (error) {
-      this.setState({ error: error.message });
-    } finally {
-      this.setState({
-        isLoading: false,
-      });
-    }
-  };
-
-  loadMoreHandler = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  toggleModal = () => {
-    this.setState(({ isModalShown }) => ({
-      isModalShown: !isModalShown,
-    }));
-  };
-
-  checkButtonShow = () => {
-    const { totalImages, page } = this.state;
+  const checkButtonShow = () => {
     return API.PER_PAGE * page < totalImages;
   };
 
-  render() {
-    const { isLoading, images, isButtonShown, isModalShown, largeImage, desc } =
-      this.state;
-    const {
-      formSubmitHandler,
-      loadMoreHandler,
-      toggleModal,
-      clearModalImageData,
-      setModalImageData,
-    } = this;
+  const toggleModal = () => {
+    setIsModalShown(!isModalShown);
+  };
 
-    return (
-      <Container>
-        <SearchBar onSubmit={formSubmitHandler} />
-        {images.length > 0 && (
-          <ImageGallery images={images} setImage={setModalImageData} />
-        )}
-        {isLoading && (
-          <Box display="flex" alignItems="center" justifyContent="center">
-            <Loader />
-          </Box>
-        )}
-        {isButtonShown && (
-          <Box display="flex" alignItems="center" justifyContent="center">
-            <Button text="Load more" buttonHandler={loadMoreHandler} />
-          </Box>
-        )}
-        {isModalShown && (
-          <Modal
-            onClose={toggleModal}
-            clearImage={clearModalImageData}
-            largeImage={largeImage}
-            desc={desc}
-          />
-        )}
-      </Container>
-    );
-  }
-}
+  const loadMoreHandler = () => {
+    setPage(page + 1);
+  };
+
+  const loadPictures = async () => {
+    try {
+      setIsLoading(true);
+      const { hits, totalHits } = await API.fetchImages(search, page);
+      setImages([...images, ...hits]);
+      setTotalImages(totalHits);
+      setIsButtonShown(checkButtonShow());
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (search !== '') {
+      loadPictures();
+    }
+  }, [page, search]);
+
+  useEffect(() => {
+    setIsButtonShown(checkButtonShow());
+  }, [images]);
+
+  return (
+    <Container>
+      <SearchBar onSubmit={formSubmitHandler} />
+      {images.length > 0 && (
+        <ImageGallery images={images} setImage={setModalImageData} />
+      )}
+      {isLoading && (
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <Loader />
+        </Box>
+      )}
+      {isButtonShown && (
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <Button text="Load more" buttonHandler={loadMoreHandler} />
+        </Box>
+      )}
+      {isModalShown && (
+        <Modal
+          onClose={toggleModal}
+          clearImage={clearModalImageData}
+          largeImage={largeImage}
+          desc={desc}
+        />
+      )}
+    </Container>
+  );
+};
+
+App.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      largeImageURL: PropTypes.string,
+      previewURL: PropTypes.string,
+      tags: PropTypes.string.isRequired,
+    })
+  ),
+  totalImages: PropTypes.number,
+  search: PropTypes.string,
+  isLoading: PropTypes.bool,
+  error: PropTypes.string,
+  isButtonShown: PropTypes.bool,
+  page: PropTypes.number,
+};
 
 export default App;
